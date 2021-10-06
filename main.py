b55fbf3077eee9370bf51e2286710ed0e9e652e5
Justus-Onyancha <justus.onyancha@moringaschoool.com>
2021-10-06 18:08:34 +0300

    This is our first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# if/elif/else
+
+height = input("Please input your height: ")
+
+if int(height) < 2:
+    print("You cannot model")
+ 
+elif int(height) == 2:
+    print("you are great for modelling")
+
+elif 
+
+elif
+
+else:
+    print("you can model")
